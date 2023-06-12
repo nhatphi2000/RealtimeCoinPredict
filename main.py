7dd7b696c1abb77edca13f51a339ea64b2cb31bf
Nhat Phi <nhatphi2106@gmail.com>
2023-06-13 01:41:59 +0700

    add RNN method and loading screen

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import dash_html_components as html
 import plotly.graph_objs as go
 from crypto_data import get_formatted_coin_list
 from predict_data import predictLSTM
+from predict_data_RNN import predict_RNN
 from dash.dependencies import Input, Output
 
 app = dash.Dash()
@@ -11,6 +12,7 @@ server = app.server
 # Example usage
 
 train, valid, predict, df = predictLSTM("bitcoin")
+trainRNN, validRNN, predictRNN, dfRNN = predict_RNN("bitcoin")
 coinList=get_formatted_coin_list() #lay danh sach coin, tuy nhien bi gioi han lan get api
 
 
@@ -20,9 +22,8 @@ app.layout = html.Div([
     html.H1("Stock Price Analysis Dashboard", style={"textAlign": "center"}),
 
     dcc.Tabs(id="tabs", children=[
-        dcc.Tab(label='Bitcoin USD Stock Data', children=[
+        dcc.Tab(label="Actual Coin closing price", children=[
             html.Div([
-                html.H2("Actual BTC closing price", style={"textAlign": "center"}),
 
                 html.Div([
                     html.Label("Select a coin:"),
@@ -33,67 +34,86 @@ app.layout = html.Div([
                     )
                 ], style={"margin-bottom": "20px"}),
 
-                dcc.Graph(
-                    id="actual-graph",
-                    figure={
-                        "data": [
-                            go.Scatter(
-                                x=train.index,
-                                y=train["Close"],
-                                mode='lines+markers',
-                                name="Close Price",
-                            )
-                        ],
-                        "layout": go.Layout(
-                            title='Actual Closing Price',
-                            xaxis={'title': 'Date'},
-                            yaxis={'title': 'Closing Rate (USD)'}
-                        )
-                    }
-                ),
-                 dcc.Graph(
-                    id="predict-graph",
-                    figure={
-                        "data": [
-                            go.Scatter(
-                                x=valid.index,
-                                y=valid["Close"],
-                                mode='lines+markers',
-                                line=dict(color="#0000ff"),
-                                name="Close Price",
-                            ),
-                            go.Scatter(
-                                x=valid.index,
-                                y=valid["Predictions"],
-                                mode='lines+markers',
-                                line=dict(color="#ffe476"),
-                                name="Predictions Price"
-                            )
-                        ],
-                        "layout": go.Layout(
-                            title='Comparision Predicted Closing Price',
-                            xaxis={'title': 'Date'},
-                            yaxis={'title': 'Closing Rate (USD)'}
-                        )
-                    }
-                ),
-                 dcc.Graph(
-                    id="next-predict-graph",
-                    figure={
-                        "data": [
-                            go.Bar(
-                                x=["LSTM"],
-                                y=predict["Predictions"],
-                                name="LSTM",
-                                width=0.5
-                            )
-                        ],
-                        "layout": go.Layout(
-                            title='Predicted Next Day Closing Price',
-                            xaxis={'title': 'Bar Name'},
-                            yaxis={'title': 'Closing Rate (USD)'}
-                        )
-                    }
+                dcc.Loading(
+                    id="loading-graphs",
+                    type="circle",
+                     children=[
+                         dcc.Graph(
+                            id="actual-graph",
+                            figure={
+                                "data": [
+                                    go.Scatter(
+                                        x=train.index,
+                                        y=train["Close"],
+                                        mode='lines+markers',
+                                        name="Close Price",
+                                    )
+                                ],
+                                "layout": go.Layout(
+                                    title='Actual Closing Price',
+                                    xaxis={'title': 'Date'},
+                                    yaxis={'title': 'Closing Rate (USD)'}
+                                )
+                            }
+                        ),
+                        dcc.Graph(
+                            id="predict-graph",
+                            figure={
+                                "data": [
+                                    go.Scatter(
+                                        x=valid.index,
+                                        y=valid["Close"],
+                                        mode='lines+markers',
+                                        line=dict(color="#0000ff"),
+                                        name="Close Price",
+                                    ),
+                                    go.Scatter(
+                                        x=valid.index,
+                                        y=valid["Predictions"],
+                                        mode='lines+markers',
+                                        line=dict(color="orange"),
+                                        name="LSTM Predictions Price"
+                                    ),
+                                    go.Scatter(
+                                        x=validRNN.index,
+                                        y=validRNN["Predictions"],
+                                        mode='lines+markers',
+                                        line=dict(color="green"),
+                                        name="RNN Predictions Price"
+                                    )
+                                ],
+                                "layout": go.Layout(
+                                    title='Comparision Predicted Closing Price',
+                                    xaxis={'title': 'Date'},
+                                    yaxis={'title': 'Closing Rate (USD)'}
+                                )
+                            }
+                        ),
+                        dcc.Graph(
+                            id="next-predict-graph",
+                            figure={
+                                "data": [
+                                    go.Bar(
+                                        x=["LSTM"],
+                                        y=predict["Predictions"],
+                                        name="LSTM",
+                                        marker=dict(color="orange")
+                                    ),
+                                    go.Bar(
+                                        x=["RNN"],
+                                        y=predictRNN["Predictions"],
+                                        name="RNN",
+                                        marker=dict(color="green")
+                                    )
+                                ],
+                                "layout": go.Layout(
+                                    title='Predicted Next Day Closing Price',
+                                    xaxis={'title': 'Bar Name'},
+                                    yaxis={'title': 'Closing Rate (USD)'}
+                                )
+                            }
+                        ), 
+                     ]
                 ),
             ])
         ]),
@@ -108,6 +128,7 @@ app.layout = html.Div([
 )
 def update_graphs(coin):
     train, valid, predict, df = predictLSTM(coin)
+    trainRNN, validRNN,predictRNN, dfRNN = predict_RNN(coin)
 
     actual_figure = {
         "data": [
@@ -137,8 +158,15 @@ def update_graphs(coin):
                 x=valid.index,
                 y=valid["Predictions"],
                 mode='lines+markers',
-                name="Predictions Price",
+                name="LSTM Predictions Price",
             ),
+            go.Scatter(
+                x=validRNN.index,
+                y=validRNN["Predictions"],
+                mode='lines+markers',
+                name="RNN Predictions Price",
+            ),
+            
         ],
         "layout": go.Layout(
             title='Comparision Predicted Closing Price',
@@ -153,7 +181,13 @@ def update_graphs(coin):
                 x=["LSTM"],
                 y=predict["Predictions"],
                 name="LSTM",
-                width=0.5
+                marker=dict(color="orange")
+            ),
+            go.Bar(
+                x=["RNN"],
+                y=predictRNN["Predictions"],
+                name="RNN",
+                marker=dict(color="green")
             )
         ],
         "layout": go.Layout(
---
+++ predict_data_RNN.py
@@ -0,0 +1,125 @@
+from keras.models import Sequential
+from keras.models import load_model
+from keras.layers import LSTM,Dropout,Dense,SimpleRNN
+from crypto_data import get_historical_prices
+from sklearn.preprocessing import MinMaxScaler
+from datetime import datetime, timedelta
+import numpy as np
+import pandas as pd
+
+def predict_RNN(coinname):
+
+    time_step=60
+    coin_name = coinname
+    vs_currency = "usd"
+    days = 1200
+
+    cryptoData = get_historical_prices(coin_name, vs_currency, days)
+    df = pd.DataFrame(cryptoData, columns=['Date','Close','ROC'])
+    df["Date"]=pd.to_datetime(df.Date,format="%Y-%m-%d")
+
+
+    df.index=df['Date']
+    data=df.sort_index(ascending=True,axis=0)
+    new_dataset=pd.DataFrame(index=range(0,len(df)),columns=['Date','Close'])
+    for i in range(0,len(data)):
+        new_dataset["Date"][i]=data['Date'][i]
+        new_dataset["Close"][i]=data["Close"][i]    
+
+    scaler=MinMaxScaler(feature_range=(0,1))
+    new_dataset.index=new_dataset.Date
+    new_dataset.drop("Date",axis=1,inplace=True)
+    final_dataset=new_dataset.values
+
+    train_data=final_dataset[0:1000,:]
+    valid_data=final_dataset[1000:,:]
+
+    scaled_data=scaler.fit_transform(final_dataset)
+    x_train_data,y_train_data=[],[]
+    for i in range(60,len(train_data)):
+        x_train_data.append(scaled_data[i-60:i,0])
+        y_train_data.append(scaled_data[i,0])
+        
+    x_train_data,y_train_data=np.array(x_train_data),np.array(y_train_data)
+    x_train_data=np.reshape(x_train_data,(x_train_data.shape[0],x_train_data.shape[1],1))
+
+    print(new_dataset)
+
+    regressor = Sequential()
+
+    # adding first RNN layer and dropout regulatization
+    regressor.add(SimpleRNN(units = 60, activation = "tanh", return_sequences = True, input_shape = (x_train_data.shape[1],1)))
+    regressor.add(Dropout(0.2))
+    # adding second RNN layer and dropout regulatization
+    regressor.add(SimpleRNN(units = 60))
+    regressor.add(Dropout(0.2))
+
+    # adding the output layer
+    regressor.add(Dense(units = 1))
+
+    # compiling RNN
+    regressor.compile(optimizer = "adam", loss = "mean_squared_error", metrics = ["accuracy"])
+
+    # fitting the RNN
+    regressor.fit(x_train_data,y_train_data,epochs=1,batch_size=1,verbose=2)
+
+
+    # Get today's date
+    start_date = datetime.now().date()
+    start_date = start_date + timedelta(days=1)
+
+    # Create an empty list to store the dictionaries
+    nextday = []
+
+    # Generate the next 5 days
+    for i in range(1):
+        # Create a dictionary with the keys "Date" and "Predictions"
+        day_dict = {"Date": start_date, "Predictions": None}
+        
+        # Add the dictionary to the list
+        nextday.append(day_dict)
+        
+        # Increment the date by 1 day
+        start_date += timedelta(days=1)
+
+    # Create a DataFrame from the list of dictionaries
+
+    Predict = pd.DataFrame(nextday)
+    Predict.index=Predict.Date
+    Predict.drop("Date",axis=1,inplace=True)
+
+    #predict 1 day later
+    X_input = new_dataset.iloc[-time_step:].Close.values               # getting last 60 rows and converting to array
+    X_input = scaler.fit_transform(X_input.reshape(-1,1))      # converting to 2D array and scaling
+    X_input = np.reshape(X_input, (1,time_step,1))                    # reshaping : converting to 3D array
+    RNN_prediction = scaler.inverse_transform(regressor.predict(X_input))
+    Predict["Predictions"][0] = RNN_prediction[0][0]
+
+
+
+    inputs_data=new_dataset[len(new_dataset)-len(valid_data)-60:].values
+    inputs_data=inputs_data.reshape(-1,1)
+    inputs_data=scaler.transform(inputs_data)
+
+    X_test=[]
+    for i in range(60,inputs_data.shape[0]):
+        X_test.append(inputs_data[i-60:i,0])
+        
+    X_test=np.array(X_test)
+    X_test=np.reshape(X_test,(X_test.shape[0],X_test.shape[1],1))
+    print("do dai ne:",len(X_test))
+
+    predicted_closing_price=regressor.predict(X_test)
+    predicted_closing_price=scaler.inverse_transform(predicted_closing_price)
+
+    train_data=new_dataset[:1000]
+    valid_data=new_dataset[1000:]
+    valid_data["Predictions"]=predicted_closing_price
+
+
+    
+    return train_data, valid_data, Predict, df
+
+
+
+
